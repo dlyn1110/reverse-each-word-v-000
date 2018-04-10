0bd1d32a9878dca7bc4cab6014b8ec9bd6497048
Dani Lyn <dlyn1110@gmail.com>
2018-04-10 18:04:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
-  new_sentence.collect do | reversed|
+  new_sentence.collect do |reversed|
     reversed.reverse!
  end
- new_sentence.join( " ")
+ new_sentence.join(" ")
 end
